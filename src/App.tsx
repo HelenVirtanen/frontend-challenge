fc11478237c43253c015528578f8ea4efd359e07
Helen Virtanen <helenvirtanen28@gmail.com>
2026-04-07 15:33:34 +0300

    chore: initialize project with React + TypeScript + Vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>Все котики</li>
+            <li>Любимые котики</li>
+          </ul>
+        </nav>
+      </header>
+      <section>Каталог котиков</section>
+    </>
+  );
+}
+
+export default App;
